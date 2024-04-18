49dbfbab7af2caa4a6068bdae52f0ecdc6e32851
Sanja Krsteska <sanjakrsteska@yahoo.com>
2024-04-18 19:07:24 +0200

    BusinessOwner, Salon Models Added

---
+++ backend/frizer/src/main/java/mk/frizer/model/BusinessOwner.java
@@ -0,0 +1,7 @@
+package mk.frizer.model;
+
+import java.util.List;
+
+public class BusinessOwner extends BaseUser {
+    List<Salon> saloons;
+}
---
+++ backend/frizer/src/main/java/mk/frizer/model/Salon.java
@@ -0,0 +1,12 @@
+package mk.frizer.model;
+
+public class Salon {
+    private String name;
+    private String description;
+    private String location;
+    private String phoneNumber;
+    // TODO Lista od termini, frizeri, uslugi, sliki
+    private BusinessOwner owner;
+
+
+}
