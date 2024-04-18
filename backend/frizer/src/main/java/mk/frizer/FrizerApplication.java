ad67577cf335aeeb5e62c88abc7a478cfa7a774d
Dario <master15820@gmail.com>
2024-04-18 18:40:27 +0200

    Added base project structure and packages

---
+++ backend/frizer/src/main/java/mk/frizer/FrizerApplication.java
@@ -0,0 +1,13 @@
+package mk.frizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrizerApplication.class, args);
+	}
+
+}
