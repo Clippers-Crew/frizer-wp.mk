d69d3c4d2c2b23f02ca386721897431aea82a3b7
Sanja Krsteska <sanjakrsteska@yahoo.com>
2024-04-23 20:58:34 +0200

    Added events for creating a salon.

---
+++ backend/frizer/src/main/java/mk/frizer/listeners/SalonEventHandler.java
@@ -0,0 +1,25 @@
+package mk.frizer.listeners;
+
+import mk.frizer.model.BusinessOwner;
+import mk.frizer.model.Salon;
+import mk.frizer.model.events.SalonCreatedEvent;
+import mk.frizer.model.exceptions.UserNotFoundException;
+import mk.frizer.service.BusinessOwnerService;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalonEventHandler {
+    private final BusinessOwnerService businessOwnerService;
+
+    public SalonEventHandler(BusinessOwnerService businessOwnerService) {
+        this.businessOwnerService = businessOwnerService;
+    }
+
+    @EventListener
+    public void onProductCreated(SalonCreatedEvent event) {
+        //TODO
+        Salon salon = (Salon)event.getSource();
+        businessOwnerService.addSalonToBusinessOwner(salon.getOwner().getId(), salon);
+    }
+}
