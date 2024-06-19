37a51fcf9f20580dbf8b7931bda6231880e10c6c
Dario <master15820@gmail.com>
2024-06-19 23:56:10 +0200

    Little merge fixes

--- backend/frizer/src/main/java/mk/frizer/model/dto/BaseUserAddDTO.java
+++ backend/frizer/src/main/java/mk/frizer/model/dto/BaseUserAddDTO.java
@@ -3,6 +3,7 @@ package mk.frizer.model.dto;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
--- backend/frizer/src/main/java/mk/frizer/service/impl/EmployeeServiceImpl.java
+++ backend/frizer/src/main/java/mk/frizer/service/impl/EmployeeServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -38,6 +39,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(EmployeeNotFoundException::new);
         return Optional.of(employee);
+    }
+
+    @Override
+    public List<Employee> getEmployeesForSalon(Long id) {
+        Salon salon = salonRepository.findById(id)
+                .orElseThrow(SalonNotFoundException::new);
+
+        List<Employee> employees =  employeeRepository.findAll().stream().filter(e->e.getSalon().equals(salon))
+                .collect(Collectors.toList());
+        return employees;
     }
 
     @Override
@@ -79,101 +90,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     @Transactional
-    public Optional<Employee> addHistoryAppointmentForEmployee(Appointment appointment) {
-        Employee employee = appointment.getEmployee();
-        employee.getAppointmentsActive().remove(appointment);
-        employee.getAppointmentsHistory().add(appointment);
-        return Optional.of(employeeRepository.save(employee));
-    }
-}
-package mk.frizer.service.impl;
-
-import mk.frizer.model.*;
-import mk.frizer.model.dto.EmployeeAddDTO;
-import mk.frizer.model.enums.Role;
-import mk.frizer.model.exceptions.*;
-import mk.frizer.repository.*;
-import mk.frizer.service.EmployeeService;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-@Service
-public class EmployeeServiceImpl implements EmployeeService {
-    private final EmployeeRepository employeeRepository;
-    private final AppointmentRepository appointmentRepository;
-    private final BaseUserRepository baseUserRepository;
-    private final ReviewRepository reviewRepository;
-    private final SalonRepository salonRepository;
-
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository, AppointmentRepository appointmentRepository, BaseUserRepository baseUserRepository, ReviewRepository reviewRepository, SalonRepository salonRepository) {
-        this.employeeRepository = employeeRepository;
-        this.appointmentRepository = appointmentRepository;
-        this.baseUserRepository = baseUserRepository;
-        this.reviewRepository = reviewRepository;
-        this.salonRepository = salonRepository;
-    }
-
-    @Override
-    public List<Employee> getEmployees() {
-        return employeeRepository.findAll();
-    }
-
-    @Override
-    public Optional<Employee> getEmployeeById(Long id) {
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(EmployeeNotFoundException::new);
-        return Optional.of(employee);
-    }
-
-    @Override
-    public List<Employee> getEmployeesForSalon(Long id) {
-        Salon salon = salonRepository.findById(id)
-                .orElseThrow(SalonNotFoundException::new);
-
-       List<Employee> employees =  employeeRepository.findAll().stream().filter(e->e.getSalon().equals(salon))
-                .collect(Collectors.toList());
-       return employees;
-    }
-
-    @Override
-    public Optional<Employee> createEmployee(EmployeeAddDTO employeeAddDTO) {
-        Optional<Employee> employee = employeeRepository.findById(employeeAddDTO.getUserId());
-        Salon salon = salonRepository.findById(employeeAddDTO.getSalonId())
-                .orElseThrow(SalonNotFoundException::new);
-        if(employee.isEmpty()){
-            BaseUser baseUser = baseUserRepository.findById(employeeAddDTO.getUserId())
-                    .orElseThrow(UserNotFoundException::new);
-
-            employee = Optional.of(new Employee(baseUser, salon));
-            baseUser.setRole(Role.ROLE_EMPLOYEE);
-            baseUserRepository.save(baseUser);
-        }
-        employee.get().setSalon(salon);
-        return Optional.of(employeeRepository.save(employee.get()));
-    }
-
-    @Override
-    public Optional<Employee> deleteEmployeeById(Long id) {
-        Optional<Employee> employee = employeeRepository.findById(id);
-        if(employee.isEmpty())
-            throw new EmployeeNotFoundException();
-        appointmentRepository.deleteAll(employee.get().getAppointmentsActive());
-        employeeRepository.deleteById(id);
-        return employee;
-
-    }
-
-    @Override
-    public Optional<Employee> addActiveAppointmentForEmployee(Appointment appointment) {
-        Employee employee = appointment.getEmployee();
-        employee.getAppointmentsActive().add(appointment);
-        return Optional.of(employeeRepository.save(employee));
-    }
-
-    @Override
     public Optional<Employee> addHistoryAppointmentForEmployee(Appointment appointment) {
         Employee employee = appointment.getEmployee();
         employee.getAppointmentsActive().remove(appointment);
